9e2423debac9d3e22715eb0a2fdf1ab6ddc14de8
Colton Savage <csavage1994@gmail.com>
2015-12-03 12:44:46 -0600

    Added edge cases along with array functionality

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,50 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var stringified = '';
+  //base case
+  if(typeof obj !== 'object'){
+
+  	if(typeof obj === 'string'){
+   		stringified += '"' + obj + '"'; 		
+  	} else if(typeof obj !== 'number' && typeof obj !== 'boolean'){
+  		stringified += '"' + obj + '"';
+  	} else{
+  		stringified += obj;
+  	}
+
+  }
+  else {
+  	if(obj === null){
+  		stringified += obj;
+  	} else if(Array.isArray(obj)){
+  		if(obj[0] === undefined){
+  			stringified += '[]';  		
+  		}
+  		if(obj.length > 0){
+  			stringified += '[';
+  			for(var i = 0; i < obj.length; i++){
+  				if(Array.isArray(obj[i])){
+  					stringified += stringifyJSON(obj[i]);
+  				}
+	  				else if(typeof obj[i] === 'string'){
+	  					stringified += '"' + obj[i] + '"';
+	  				} else{
+	  					stringified += obj[i];
+	  				}
+	  				if(i !== obj.length - 1){
+	  					stringified += ',';
+	  				}
+	  			}
+  			stringified += ']';
+  		}
+  	} else{
+  			  		console.log('here');
+	  	_.each(obj, function(key, value){
+
+	  		stringified += '"' + key + '":' + obj[key];
+	  	});
+	  }
+  }
+  return stringified;
 };
